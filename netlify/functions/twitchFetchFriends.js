e57c80bf7e6fb11e86979c44f80b238e1366d683
TAK <dubois13.romain@gmail.com>
2025-09-03 12:23:52 +0200

    Add update to fetch friends from twitch and added live status api

---
+++ netlify/functions/twitchFetchFriends.js
@@ -0,0 +1,86 @@
+const SUsers = ["twistedtxx", "angelicdemon325", "plaguedemonn", "step_milkman", "allmondmlk", "rocka_timez", "0wabi0", "callzack", "slouchyharbor", "iiplaguedoctress", "the_hunter146", "starfirexox420_ttv", "duhkoterz", "itsbenzo_", "quek1tty", "bindbleed"];
+const nSUsers = ["zahra33op", "beeleavee", "caitlinharrisonx", "exiledxiv", "cozzslaps", "sugardaddywes"];
+
+const nicknames = {
+  "angelicdemon325": "Angel",
+  "twistedtxx": "Twisted",
+  "step_milkman": "Milk",
+  "allmondmlk": "Almond",
+  "plaguedemonn": "Plague",
+  "bindbleed": "Bind",
+  "rocka_timez": "Rocka",
+  "0wabi0": "Wabi",
+  "callzack": "Zack",
+  "itsbenzo_": "Benzo",
+  "quek1tty": "Kitty",
+  "beeleavee": "Bee",
+  "caitlinharrisonx": "Caitlin",
+  "exiledxiv": "Exi",
+  "zahra33op": "My Brother",
+  "slouchyharbor": "Slouchy",
+  "starfirexox420_ttv": "Star",
+  "duhkoterz": "Koterz",
+  "cozzslaps": "Cozz",
+  "sugardaddywes": "Wes",
+  "the_hunter146": "Hunter",
+  "iiplaguedoctress": "Plaguess"
+};
+
+const exceptUsers = {
+  "cozzslaps": "https://cdn.discordapp.com/avatars/1172873389514432562/09cb2ec45b49f6e7f44d9d41259f9849.webp?size=512"
+};
+
+export async function handler(event) {
+  const CLIENT_ID = process.env.TWITCH_CLIENT_ID;
+  const CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET;
+
+  const tokenRes = await fetch(
+    `https://id.twitch.tv/oauth2/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`,
+    { method: "POST" }
+  );
+  const tokenData = await tokenRes.json();
+  const accessToken = tokenData.access_token;
+
+  const fetchUsers = async (users, streamer) => {
+    const results = [];
+    for (const user of users) {
+      try {
+        const userRes = await fetch(`https://api.twitch.tv/helix/users?login=${user}`, {
+          headers: {
+            "Client-ID": CLIENT_ID,
+            "Authorization": `Bearer ${accessToken}`
+          }
+        });
+        const data = await userRes.json();
+        if (data.data && data.data[0]) {
+          const u = data.data[0];
+          results.push({
+            login: u.login,
+            display_name: u.display_name,
+            profile_image_url: exceptUsers[user] || u.profile_image_url,
+            nickname: nicknames[user] || null,
+            isStreamer: streamer
+          });
+        }
+      } catch (err) {
+        console.error(`Error fetching ${user}:`, err);
+      }
+    }
+    return results;
+  };
+  const fetchLiveStatus = async(GHXSTAK) => {
+    const streamRes = await fetch(`https://api.twitch.tc/helix/streams?${user}`)
+  }
+  const streamerData = await fetchUsers(SUsers, true);
+  const nonStreamerData = await fetchUsers(nSUsers, false);
+  const liveStatus = await fetchLiveStatus()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      
+      streamers: streamerData,
+      nonStreamers: nonStreamerData
+    })
+  };
+}
---
+++ netlify/functions/twitchLiveStatus.js
@@ -0,0 +1,50 @@
+export async function handler(event) {
+  const CLIENT_ID = process.env.TWITCH_CLIENT_ID;
+  const CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET;
+  const USERNAME = "ghxstak";
+
+  const tokenRes = await fetch(
+    `https://id.twitch.tv/oauth2/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`,
+    { method: "POST" }
+  );
+  const tokenData = await tokenRes.json();
+  const accessToken = tokenData.access_token;
+
+  const fetchLiveStatus = async(user) => {
+    try {
+          const userRes = await fetch(`https://api.twitch.tv/helix/users?login=${user}`, {
+            headers: {
+              "Client-ID": CLIENT_ID,
+              "Authorization": `Bearer ${accessToken}`
+            }
+          });
+          const userData = await userRes.json();
+          const u = userData.data[0];
+
+          const streamRes = await fetch(`https://api.twitch.tc/helix/streams?${USERNAME}`{
+            headers: {
+              "Client-ID": CLIENT_ID,
+              "Authorization": `Bearer ${accessToken}`
+            }
+          });
+          const streamData = await streamRes.json();
+
+          return {
+            login: u.login,
+            display_name: u.display_name,
+            profile_image_url: u.profile_image_url,
+            isLive: streamData.data && streamData.data.length> 0
+          };
+    } catch(err){
+      console.error("Error fetching Twitch stauts", err);
+      return {error: "Unable to fetch status"};
+    }
+  }
+};
+
+const liveStatus = await fetchLiveStatus(USERNAME);
+
+return {
+  statusCode: 200,
+  body: JSON.stringify(liveStatus)
+};
