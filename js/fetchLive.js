54a97bfd738efb39d59d38d8a5528ff8f36e6b5b
TAK <dubois13.romain@gmail.com>
2025-09-06 20:26:32 +0200

    changed innerHTML to textContent

--- js/fetchLive.js
+++ js/fetchLive.js
@@ -5,13 +5,12 @@ fetch("/.netlify/functions/twitchLiveStatus")
   .then(res => res.json())
   .then(data => {
     console.log(data);
-    if isLive == "true" {
-      liveIcon.style.display = "inline",
-      liveText.innerHTML = "Join the stream"
-    }
-    else {
-      liveIcon.style.display = "none",
-      liveText.innerHTML = "Check my Twitch"
+    if (data.isLive) {
+      liveIcon.style.display = "inline";
+      liveText.textContent = "Join the stream";
+    } else {
+      liveIcon.style.display = "none";
+      liveText.textContent = "Check my Twitch";
     }
   })
   .catch(err => console.error(err));
