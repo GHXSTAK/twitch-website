e1073b2dbe425a5e67a0556ddd2af5bd52378de3
TAK <dubois13.romain@gmail.com>
2025-09-06 20:24:32 +0200

    added live frontend

--- js/fetchLive.js
+++ js/fetchLive.js
@@ -1,8 +1,17 @@
+const liveIcon = document.getElementById("live-icon");
+const liveText = document.getElementById("join-the-stream");
+
 fetch("/.netlify/functions/twitchLiveStatus")
   .then(res => res.json())
   .then(data => {
     console.log(data);
-
-    // Streamers
+    if isLive == "true" {
+      liveIcon.style.display = "inline",
+      liveText.innerHTML = "Join the stream"
+    }
+    else {
+      liveIcon.style.display = "none",
+      liveText.innerHTML = "Check my Twitch"
+    }
   })
   .catch(err => console.error(err));
